aaa4ffab837699c659de03a2b9b5df1c96adb660
Hafis <hafisk469@gmail.com>
2023-09-27 11:22:15 +0530

    first commit

---
+++ components/About.tsx
@@ -0,0 +1,45 @@
+'use client'
+import React, { useEffect } from 'react'
+import SectionHeading from './SectionHeading'
+import { motion } from 'framer-motion'
+import { useSectionInView } from '@/lib/hooks'
+
+const About = () => {
+  const { ref } = useSectionInView('About')
+
+  return <motion.section className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28' id='about'
+    initial={{ opacity: 0, y: 100 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ delay: 0.175 }}
+    ref={ref}
+  >
+    <SectionHeading>
+      about
+    </SectionHeading>
+    <p className='mb-3'>
+      My journey began with online tutorials and courses that laid the foundation for my {" "}
+      <span className="font-medium">skills</span>, I decided to pursue my
+      passion for programming. I did hands-on projects and learned{" "}
+      <span className="font-medium">web development</span>.{" "}
+      <span className="italic">My favorite part of programming</span> is the
+      problem-solving aspect. I <span className="underline">love</span> the
+      feeling of finally figuring out a solution to a problem. My core stack
+      is{" "}
+      <span className="font-medium">
+        React, Next.js and Node.js
+      </span>
+      . I am also familiar with TypeScript. I am always looking to
+      learn new technologies. I am currently looking for a{" "}
+      <span className="font-medium">full-time position</span> as a frontend web development.
+    </p>
+    <p>
+      <span className="italic">When I'm not coding</span>, I enjoy playing
+      video games and watching movies. I also enjoy{" "}
+      <span className="font-medium">learning new things</span>. I am currently
+      learning about{" "}
+      <span className="font-medium">content creation</span>.
+    </p>
+  </motion.section>
+}
+
+export default About
---
+++ lib/data.ts
@@ -0,0 +1,83 @@
+import React from "react";
+import { LuGraduationCap } from "react-icons/lu";
+import youtubeCloneImg from "@/public/youtubeClone.png";
+import foodRecipeApp from "@/public/foodRecipeApp.png";
+import fitto from "@/public/fitto.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Self-taught",
+    location: "Kerala, IN",
+    description:
+      "Learning and exploring Web development to build a career in tech by utalizing free online resources and applying for oppertunity.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2023 - Present",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "YoutubeClone",
+    description: "A YouTube clone app with a modern UI. It have features of the original YouTube app, such as the ability to search for videos, watch videos in full screen.",
+    tags: ["React", "Rapid API", "Material UI", "Git"],
+    imageUrl: youtubeCloneImg,
+    link:'https://youtube-clone-hafis.netlify.app/',
+  },
+  {
+    title: "Delicious",
+    description:
+      "Delicious is a food recipe app that contains a wide variety of recipes. the app includes images of each recipe to help you get inspired.",
+    tags: ["React", "Material UI", "Spoonacular API", "Git"],
+    imageUrl: foodRecipeApp,
+    link:'https://food-recipeee.netlify.app/',
+  },
+  {
+    title: "Fitto",
+    description:
+      "Fitto is a fitness app that provides users with access to thousands of exercises, video demonstrations, and personalized training plans.",
+    tags: ["React", "Material UI", "Rapid API", "Git"],
+    imageUrl: fitto,
+    link:'https://fitto-fitness.netlify.app/',
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Redux",
+  "Framer Motion",
+] as const;
