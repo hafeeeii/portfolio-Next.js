87b800b3855abd4c5e26dbdc6e0a6c1c531fe12d
Hafis <hafisk469@gmail.com>
2023-10-19 01:48:28 +0530

    updated project section

--- lib/data.ts
+++ lib/data.ts
@@ -3,6 +3,7 @@ import { LuGraduationCap } from "react-icons/lu";
 import youtubeCloneImg from "@/public/youtubeClone.png";
 import foodRecipeApp from "@/public/foodRecipeApp.png";
 import fitto from "@/public/fitto.png";
+import filmverse from "@/public/filmverse.png";
 
 export const links = [
   {
@@ -44,27 +45,43 @@ export const experiencesData = [
 
 export const projectsData = [
   {
+    title: "Filmverse",
+    description:
+      "Filmverse is a React-based movie website that allows users to search for movies , TV show and bookmark their favorite movies and TV shows.",
+    tags: [
+      "React",
+      "TMDB API",
+      "Tailwind",
+      "Firebase",
+      "Redux",
+      "Framer Motion",
+    ],
+    imageUrl: filmverse,
+    link: "https://filmverse-ink.vercel.app/",
+  },
+  {
     title: "YoutubeClone",
-    description: "A YouTube clone app with a modern UI. It have features of the original YouTube app, such as the ability to search for videos, watch videos in full screen.",
-    tags: ["React", "Rapid API", "Material UI", "Git"],
+    description:
+      "A YouTube clone app with a modern UI. It have features of the original YouTube app, such as the ability to search for videos.",
+    tags: ["React", "Rapid API", "Material UI"],
     imageUrl: youtubeCloneImg,
-    link:'https://youtube-clone-hafis.netlify.app/',
+    link: "https://youtube-clone-hafis.netlify.app/",
   },
   {
     title: "Delicious",
     description:
       "Delicious is a food recipe app that contains a wide variety of recipes. the app includes images of each recipe to help you get inspired.",
-    tags: ["React", "Material UI", "Spoonacular API", "Git"],
+    tags: ["React", "Material UI", "Spoonacular API"],
     imageUrl: foodRecipeApp,
-    link:'https://food-recipeee.netlify.app/',
+    link: "https://food-recipeee.netlify.app/",
   },
   {
     title: "Fitto",
     description:
       "Fitto is a fitness app that provides users with access to thousands of exercises, video demonstrations, and personalized training plans.",
-    tags: ["React", "Material UI", "Rapid API", "Git"],
+    tags: ["React", "Material UI", "Rapid API"],
     imageUrl: fitto,
-    link:'https://fitto-fitness.netlify.app/',
+    link: "https://fitto-fitness.netlify.app/",
   },
 ] as const;
 
